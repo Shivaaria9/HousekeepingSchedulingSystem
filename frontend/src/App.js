5d5c97574ab83a8922c117d40147e46235a8bed8
Shivaaria9 <shivaaria@gmail.com>
2024-02-07 17:32:21 +0530

    App js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-// import logo from "./logo.svg";
-import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-// import Login from "./components/Login/Login";
+import logo from './logo.svg';
+import Login from './Component/Login';
+import Dashboard from './Component/Dashboard';
+import AdminDash from './Component/AdminDash';
+import ForgotPassword from './Component/ForgotPassword';
+import { BrowserRouter as Router,Routes,Route,Link } from 'react-router-dom';
+//import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      {/* <Login /> */}
+    <div >
+      <Router>
+        <Routes>
+          <Route path='/' element={<Login/>}/>
+          <Route path='/Dashboard' element={<Dashboard/>}/>
+          <Route path='/AdminDash' element={<AdminDash/>}/>
+          <Route path='/ForgotPassword' element={<ForgotPassword/>}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
