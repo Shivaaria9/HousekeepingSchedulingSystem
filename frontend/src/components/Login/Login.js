69d9548ccf0f831d9666853af902ae10f296f46f
Shivaaria9 <shivaaria@gmail.com>
2024-02-08 14:54:10 +0530

    login form

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,125 +1,146 @@
-import React, { useState } from 'react';
-import {Link,useNavigate} from 'react-router-dom';
-import axios from 'axios';
-import './Login.css';
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import "./Login.css";
+import axios from "axios";
 
-function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-  const [result, setResult]  = useState(""); 
-  const navigate = useNavigate(); 
-
- 
-  // Function to validate Email
-  const validateEmail = (value) => {
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    // Check if the entered credentials are for a student
-    if (email !== 'admin@gmail.com') {
-      if (!value.match(emailPattern)) {
-        setEmailError('Invalid email format');
-      } else {
-        setEmailError('');
+const LoginComponent = () => {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get("http://localhost:3005/api/students");
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
       }
     }
+
+    fetchData();
+  }, []);
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [errorField, setError] = useState("");
+  const [userData, setUserData] = useState([]);
+  const [adminStatus, setAdminStatus] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const validateEmail = (email) => {
+    const emailPattern = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return emailPattern.test(email);
   };
 
-  // Function to validate Password
-  const validatePassword = (value) => {
-    const passwordPattern = /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
-
-    // Check if the entered credentials are for a student
-    if (email !== 'admin@gmail.com') {
-      if (!value.match(passwordPattern)) {
-        setPasswordError('Password: At least 6 characters, one uppercase letter, one number, and one special character.');
-      } else {
-        setPasswordError('');
-      }
-    }
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+    setError("");
   };
 
-  const handleLogin = async () => {
-    // Validate admin credentials
-    if (email === 'admin@gmail.com' && password === 'admin@123') {
-      // Navigate to admin dashboard
-      console.log('Admin login successful!');
-      alert('Admin Login Successful');
-      navigate("/AdminDash");
+  const handlePasswordChange = (e) => {
+    const value = e.target.value;
+    setPassword(value);
+    setError("");
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (!email || !password) {
+      setError("Email or password cannot be empty");
       return;
     }
+    if (!validateEmail(email)) {
+      setError("Invalid email-id");
+      return;
+    } else {
+      Login(email, password);
+    }
+  };
+  const Login = (email, password) => {
+    setError("");
+    const user = userData.find((user) => user.email === email.toLowerCase());
 
-    // Validate student credentials
-    validateEmail(email);
-    validatePassword(password);
-
-    if (!emailError && !passwordError) {
-      // If both email and password are valid, proceed with the login
-      try {
-        let url = "http://localhost:3005/api/students/login";
-        const response = await axios.post(url, { email, password });
-        if (response.data.success) {
-          console.log('Student login successful!');
-          alert('Student Login Successful');
-          navigate("/Dashboard");
+    if (!user) {
+      setError("Unregistered Email Id");
+      return;
+    } else {
+      if (user.password === password) {
+        if (user.email === "admin@example.com") {
+          sessionStorage.setItem("UserType", "Admin");
         } else {
-          setResult(response.data.message);
-          if (response.data.message === 'User not found' || response.data.message === 'Incorrect password') {
-            alert("Please enter correct Email-Id or password");
-          }
+          sessionStorage.setItem("UserType", "Student");
         }
-      } catch (error) {
-        console.error('Error during student login:', error);
-        setErrorMessage('Error during login. Please try again.');
+        const queryString = location.search; // returns the query string from the current url
+        let strReturnUrl = new URLSearchParams(queryString).get("returnUrl");
+        if (strReturnUrl === null) {
+          strReturnUrl = "/dashboard";
+        }
+        // In real-time apps, we will get the token from the server
+        // JWT token is the popular token generation library
+        let token = "ASJDFJF87ADF8745LK4598SAD7FAJSDF45JSDLFKAS";
+        sessionStorage.setItem("user-token", token);
+        console.log(sessionStorage.getItem("UserType"));
+        navigate(strReturnUrl);
+        // navigate("/dashboard", { state: { prop1: adminStatus } });
+      } else {
+        setError("Incorrect Password");
       }
     }
   };
-  
+
   return (
     <div className="outer-container">
       <div className="container">
         <div className="form-container log-in-container">
-          <form>
-            <h1>Login</h1>
-            <input
-              type="text"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-                validateEmail(e.target.value);
-              }}
-            />
-            {emailError && <p className="error-message">{emailError}</p>}
-            <input
-              type="password"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-                validatePassword(e.target.value);
-              }}
-            />
-            {passwordError && <p className="error-message">{passwordError}</p>}
-            {errorMessage && <p className="error-message">{errorMessage}</p>}
-               <div className='links'>
+          <form className="form">
+            <h1 style={{ color: "#00796b" }}>Login</h1>
+            <div className="formgroup">
+              <input
+                className="input"
+                type="email"
+                placeholder="Enter your email-id"
+                value={email}
+                onChange={handleEmailChange}
+              />
+            </div>
+            <div className="formgroup">
+              <input
+                className="input"
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              <p className="showPassword" onClick={togglePasswordVisibility}>
+                {showPassword ? (
+                  <i class="bi bi-eye-slash"></i>
+                ) : (
+                  <i class="bi bi-eye"></i>
+                )}
+              </p>
+            </div>
+            <button type="button" onClick={handleLogin}>
+              Login
+            </button>
+            <div className='links'>
                <Link to="/ForgotPassword" style={{ textDecoration: 'none', marginTop:'15px' , color: '#19725D' }}>Forgot Password</Link>
               <label>
                 Not a Member? <Link to="/Register" style={{ textDecoration: 'none' , marginTop:'10px',color: '#19725D' }}>Sign Up</Link>
               </label>
               </div>
-              <button type="button" onClick={handleLogin}>
-              Login
-            </button>
+            {errorField && <div className="error-message">{errorField}</div>}
           </form>
         </div>
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-right">
               <h1>HouseKeeper Scheduling system</h1>
-              <p>This is the place where you can schedule your comfortable time for cleaning purposes for HouseKeeper.</p>
+              <p>
+                This is the place where you can schedule your comfortable time
+                for cleaning purposes for HouseKeeper.
+              </p>
             </div>
           </div>
         </div>
@@ -128,4 +149,4 @@ function Login() {
   );
 };
 
-export default Login;
+export default LoginComponent;
